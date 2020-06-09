364137eb6814fb6c0b2ce379bb72fd24804e973f
tiago <tiago@ticogame.com>
2020-06-09 20:05:36 -0300

    Testando o banco com pipe

--- src/controller/controllers.js
+++ src/controller/controllers.js
@@ -15,7 +15,7 @@ module.exports = {
                     value: dados.value,
                     products_count: dados.products_count
                 });
-                console.log(retorno)
+                retorno.then((result) => { console.log(result) }).catch(err=>{console.log(err)});
                 break;
 
             default:
