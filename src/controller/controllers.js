d5cc129d1145a90296832d392ceb9e3d30ebf585
tiago <tiago@ticogame.com>
2020-06-09 19:36:02 -0300

    Testando o banco

--- src/controller/controllers.js
+++ src/controller/controllers.js
@@ -1,10 +1,11 @@
 const pipe = require("pipedrive"),
-    banco = global.banco;
+    banco = require('../models/model');
 pipe.Configuration.apiToken = "83f409a0ac6fb301aa218884a3cacb9e1940a591";
 
 module.exports = {
     async deal_update(req, res) {
         let dados = req.body.current;
+        banco.DB_insert("vendas",1);
         return res.send({status:true});
     },
 
--- src/models/model.js
+++ src/models/model.js
@@ -1,2 +1,8 @@
 const banco = global.banco;
-module.exports = {}
+module.exports = {
+    async DB_insert(table,date){
+banco.collection("vendas").insert({id:1},(err,result)=>{
+    console.log(result)
+})
+    }
+}
