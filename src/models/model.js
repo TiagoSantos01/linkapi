cf8cc1e33b8d8a22cd7ff3bf75e5dde5360c746c
tiago <tiago@ticogame.com>
2020-06-09 19:46:05 -0300

    Testando o banco

--- src/models/model.js
+++ src/models/model.js
@@ -1,8 +1,8 @@
 const banco = global.banco;
 module.exports = {
     async DB_insert(table,date){
-        console.log(banco)
-banco.collection("vendas").insert({id:1},(err,result)=>{
+        console.log(global.banco)
+        global.banco.collection("vendas").insert({id:1},(err,result)=>{
     console.log(result)
 })
     }
