70fe2e02d5b6e4f32a18209ba09f36a87b95c3c4
tiago <tiago@ticogame.com>
2020-06-09 19:06:18 -0300

    mudando variavel bodyparser para jsonParser

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
 const routes = require("express").Router();
-let bodyParser = require("body-parser").json();
+let jsonParser = require("body-parser").json();
 
 routes.get('/',jsonParser, (req, res) => { res.send("ON") })
