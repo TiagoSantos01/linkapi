afbdec8371d985554e361a4c4e05680c53ec1a1f
tiago <tiago@ticogame.com>
2020-06-09 19:04:09 -0300

    Testando rotas

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
-const app=require("express").Router();
-let bodyParser=require("body-parser").json();
-module.exports={}
+const routes = require("express").Router();
+let bodyParser = require("body-parser").json();
+
+routes.get('/',jsonParser, (req, res) => { res.send("ON") })
