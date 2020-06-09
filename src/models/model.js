97b613aa4971e1c8ae123a87706d8538e495b856
tiago <tiago@ticogame.com>
2020-06-09 20:07:21 -0300

    Testando o banco com pipe

--- src/models/model.js
+++ src/models/model.js
@@ -3,10 +3,10 @@ module.exports = {
 
     async Db_negocios_insert(data) {
         let banco = global.banco;
-         banco.collection("negocios").insert(data, (err, result) => {
+      return   banco.collection("negocios").insert(data, (err, result) => {
             return true
            // return { err, result }
         })
-        return 11;
+        
     }
 }
