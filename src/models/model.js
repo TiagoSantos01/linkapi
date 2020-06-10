d6b4aaca2e9f398c886531c1dd4701159a68f9c5
tiago <tiago@ticogame.com>
2020-06-10 13:55:09 -0300

    funcoes de update no won do pipe

--- src/models/model.js
+++ src/models/model.js
@@ -11,7 +11,11 @@ module.exports = {
     },
     async Db_negocios_update(datawhere, data) {
         let banco = global.banco;
-        return banco.collection("negocios").updateOne({ datawhere }, { data })
+        retorno = await banco.collection("negocios").findOne({ _id: data._id });
+        if (retorno == null)
+            return Db_negocios_insert(data)
+        else
+            return banco.collection("negocios").updateOne({ datawhere }, { data })
     }
 
 }
