c27ee9aa7f705659f269bce6f9999bf13dd2f6ab
tiago <tiago@ticogame.com>
2020-06-10 12:55:24 -0300

    funcoes de update no won do pipe

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const routes = require("express").Router(),
 routes.get('/', jsonParser, (req, res) => { return res.send("ON") })
 
 //  POST
-routes.post('/deal/update',jsonParser, controllers.deal_update);
+routes.post('/deal/update',jsonParser,await controllers.deal_update);
 routes.post('/deal/delete',jsonParser, controllers.deal_delete);
 routes.post('/gitpull', jsonParser, controllers.git);
 
