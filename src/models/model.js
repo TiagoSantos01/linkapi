012243861a3cc8ecdbb73f48b71ed09bc218f542
tiago <tiago@ticogame.com>
2020-06-10 20:54:16 -0300

    debug no adicionamento de produtos

--- src/models/model.js
+++ src/models/model.js
@@ -19,6 +19,7 @@ module.exports = {
     async Db_produtos_Insert_List(dados) {
         let banco = global.banco;
         dados.forEach(async data => {
+            console.log("foi");
             retorno = await banco.collection("produtos").findOne({ id: data.id, deal_id: data.deal_id });
             if (!retorno)
             banco.collection("produtos").insertOne(data)
