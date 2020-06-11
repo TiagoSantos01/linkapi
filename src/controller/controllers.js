1446716cb10c7f63f90f55271fe650bf8f0faaad
tiago <tiago@ticogame.com>
2020-06-11 12:38:51 -0300

    vendo do mongo para bling

--- src/controller/controllers.js
+++ src/controller/controllers.js
@@ -33,14 +33,52 @@ module.exports = {
                     status: dados.status,
                     value: dados.value,
                     add_time: dados.add_time,
-                    update_time: dados.update_time
+                    update_time: dados.update_time,
+                    won_time: dados.won_time,
+                    owner_name: dados.owner_name
                 }).then(async (result) => {
                     controller = pipe.DealsController;
                     produtos = controller.listProductsAttachedToADeal({ id: dados.id })
                     produtos.then(async (result) => {
                         await banco.Db_produtos_Insert_List(result.data)
+                        const request = require("request"),
+                            convert = require('xml-js'),
+                            apikey = '96fe77d3ed131ef9d7f76b02314ea54aaa3468223934d4a7d1a419d6207cb5f65a38cb7d';
+
+                        let json = [];
+                        retorno = banco.collection("negocios").findOne({ id: dados.id })
+                        banco.collection("produtos").find({ deal_id: dados.id }).toArray(async (err, result) => {
+                            itens = [];
+                            await result.forEach(dados => {
+
+                                itens.push({
+                                    codigo: dados.product_id,
+                                    descricao: dados.comments || " ",
+                                    qtde: dados.quantity,
+                                    valor: dados.sum
+                                }
+
+                                )
+                            });
+                            item = { item: itens }
+                            json.pedidocompra = [];
+                            json.pedidocompra = {
+                                numeropedido: retorno.id,
+                                datacompra: retorno.won_time,
+                                dataprevista: retorno.add_time,
+                                fornecedor: {
+                                    nome: retorno.owner_name
+                                },
+                                itens: item
+                            }
+                            xml = convert.json2xml(json, { compact: true })
+                        })
+                        request.post(`https://bling.com.br/Api/v2/pedidocompra/json/&apikey=${apikey}&xml=${xml}`, {}, (error, request, result) => {
+                            res.send({ success: true, result: result }) 
+                        })
+
                     })
-                    res.send({ success: true, result: result })
+                   
                 })
                     .catch(err => {
                         res.send({ success: false, error: err })
@@ -88,7 +126,7 @@ module.exports = {
         let id = req.body.meta.id;
 
         await banco.Db_produtos_drop({ deal_id: id }).then(async () => {
-            await banco.Db_negocios_drop({ id: id,status:"drop" }).then(result => {
+            await banco.Db_negocios_drop({ id: id, status: "drop" }).then(result => {
                 res.send({ success: true, result: result })
             }).catch(err => {
                 res.send({ success: false, error: err })
